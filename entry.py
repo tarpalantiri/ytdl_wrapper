dc957b6745b8df540877434ce1e451cf24f7e269
tarpalantiri <tehseensajjadsb@gmail.com>
2020-10-21 16:39:46 +0500

    inc

--- entry.py
+++ entry.py
@@ -2,11 +2,11 @@
 
 from youtube_dl import YoutubeDL
 from os import system
-from utils import formats, helpers
+from utils import settings, helpers
 system('cls')
 
 if __name__ == '__main__':
-    with YoutubeDL(formats.AUDIO_MP3_192) as ydl:
+    with YoutubeDL(settings.AUDIO_MP3_192) as ydl:
         links = helpers.get_links()
         if links:
             ydl.download(links)
